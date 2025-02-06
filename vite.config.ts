42fd25baa3801a5c02d88a5520a03f5b7cad6068
Saturnych <169702+Saturnych@users.noreply.github.com>
2025-02-06 07:44:52 +0300

    initial sveltekit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build',
-  },
+	plugins: [sveltekit()]
 });
