0e8ba7cf0deda0dd816a99f5f8033486fa57c62e
Denis <saturnych@gmail.com>
2025-02-06 17:39:54 +0300

    Update supabase.ts

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,16 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/vars/client';
+import { APP_NAME, SUPABASE_URL, SUPABASE_ANON_KEY } from '$lib/vars/client';
 
 // Create a single supabase client for interacting with your database
-export const supabaseCreateClient = (schema: string = 'public') =>
-	createClient(SUPABASE_URL, SUPABASE_ANON_KEY, { db: { schema } });
+export const supabaseCreateClient = (schema: string = 'public') => {
+  const options = {
+    db: { schema },
+    headers: { 'x-app-name': APP_NAME },
+    autoRefreshToken: true,
+    persistSession: true,
+    detectSessionInUrl: true,
+  };
+  return createClient(SUPABASE_URL, SUPABASE_ANON_KEY, options);
+};
 
 export const supabase = supabaseCreateClient();
